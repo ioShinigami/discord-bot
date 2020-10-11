63509d09798b09931606dab5b98ec0759f220422
Shinigami <clearly.ts@protonmail.com>
2020-10-11 11:03:13 -0700

    no prog ticket.js... get-meme issue resolved

--- commands/get-meme.js
+++ commands/get-meme.js
@@ -3,18 +3,25 @@ const channelId = "763838373261082657"
 module.exports = {
 	name: 'get-meme',
 	description: 'Display info about yourself.',
-	execute(message) {
+	args: true,
+	execute(message, args) {
 		if(message.channel.id === channelId) {
-
-			;(async() => {
-				let fetched = await api("meme", true)
-				console.log(fetched); // logs the image;
-				let advanced = await api.advanced("meme", true);
+			var i;
+			message.reply('Please wait...Your memes are loading')
+			for(i = 0;  i < args[0]; i++) {
+				;(async() => {
+					let fetched = await api("meme", true)
+					console.log(fetched); // logs the image;
+					let advanced = await api.advanced("meme", true);
 				console.log(advanced); // { img: *img*, res: *response time*, };
-				message.channel.send(advanced.img)
+				setTimeout(() => {
+					message.reply(advanced.img)					
+				}, 5000);
 			})();
-			} else {
-				message.reply('This is not the correct channel for commands , please use #memes')
-			}
+		}
+		
+		} else {
+			message.reply('This is not the correct channel for commands , please use #memes')
+		}
 	},
 };
--- commands/ticket.js
+++ commands/ticket.js
@@ -4,10 +4,13 @@ module.exports = {
     description: 'Creates a Ticket Channel for User.',
     execute(message) {
         var owner = message.guild.ownerID
+        console.log("execute -> owner", owner)
         const name = message.author.username
-        const everyoneRole = message.guild.roles.cache.get(role => role.name === "Everyone");
+        console.log("execute -> name", name)
         var vN = randomTagGenerator()
         message.guild.channels.create("[SUPPORT TICKET]  :  " + vN, { type: 'text', reason: 'Support Ticket' })
+        var everyoneRole = message.member.roles.find(r => r.name === "everyone")
+        console.log("execute -> everyoneRole", everyoneRole)
         var newRole = 'support' + vN;
         message.guild.roles.create({
             data: {
@@ -18,9 +21,8 @@ module.exports = {
         })
             .then(r => {
 
-
                 ///TODO FIX THIS - cant add a role to member
-                message.addRole(message.guild.roles.find(role => role.name === newRole))
+              //  message.addRole(message.guild.roles.find(role => role.name === newRole))
 
                 /*  r.permissionOverwrites(message.author.id, { VIEW_CHANNEL: true});
                   r.permissionOverwrites(owner, {VIEW_CHANNEL: true});
