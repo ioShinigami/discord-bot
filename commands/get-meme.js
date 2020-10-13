68b4b6932ba97283ba8f1dd7a395b2eda6784824
Shinigami <clearly.ts@protonmail.com>
2020-10-13 12:43:21 -0700

    Broken Command?? :(

--- commands/get-meme.js
+++ commands/get-meme.js
@@ -1,27 +1,27 @@
-const api = require('imageapi.js');
-const channelId = "763838373261082657"
-module.exports = {
-	name: 'get-meme',
-	description: 'Display info about yourself.',
-	args: true,
-	execute(message, args) {
-		if(message.channel.id === channelId) {
-			var i;
-			message.reply('Please wait...Your memes are loading')
-			for(i = 0;  i < args[0]; i++) {
-				;(async() => {
-					let fetched = await api("meme", true)
-					console.log(fetched); // logs the image;
-					let advanced = await api.advanced("meme", true);
-				console.log(advanced); // { img: *img*, res: *response time*, };
-				setTimeout(() => {
-					message.reply(advanced.img)					
-				}, 5000);
-			})();
-		}
+// const api = require('imageapi.js');
+// const channelId = "763838373261082657"
+// module.exports = {
+// 	name: 'get-meme',
+// 	description: 'Display info about yourself.',
+// 	args: true,
+// 	execute(message, args) {
+// 		if(message.channel.id === channelId) {
+// 			var i = args[0];
+// 			message.reply('Please wait...Your memes are loading')
+// 			for(i = args[0];  i >= 0; i--) {
+// 				console.log(i)
+// 				;(async() => {
+// 					let fetched = await api("meme", true)
+// 					console.log(fetched); // logs the image;
+// 					let advanced = await api.advanced("meme", true);
+// 				console.log(advanced); // { img: *img*, res: *response time*, };
+			
+// 					message.reply(advanced.img)					
 		
-		} else {
-			message.reply('This is not the correct channel for commands , please use #memes')
-		}
-	},
-};
+// 			})();
+// 			}
+// 		} else {
+// 			message.reply('This is not the correct channel for commands , please use #memes')
+// 		}
+// 	}
+// }
