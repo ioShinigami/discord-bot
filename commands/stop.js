732a268102b003dc518aefd0a20e13f530a229d2
Shinigami <clearly.ts@protonmail.com>
2020-10-12 21:39:51 -0700

    Require Channel !stop

--- commands/stop.js
+++ commands/stop.js
@@ -1,8 +1,10 @@
+const discordChannelId = "763793192453275659";
 module.exports = {
 	name: 'stop',
     description: 'Stop current song thats playing.',
 	execute(message) {
         const voiceChannel = message.member.voice.channel;
+        if (message.channel.id === discordChannelId) {
         if(!voiceChannel) {
             return message.reply("Please join a voice channel first!").then(msg => {
                 msg.channel.delete({ timeout: 5 })
@@ -10,6 +12,8 @@ module.exports = {
             .catch(console.log(error));
         }
         voiceChannel.leave()
-		
+        } else {
+            message.reply('This is not the correct channel for commands , please use #botcommands')
+        } 
 	},
 };
